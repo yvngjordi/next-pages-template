99332faf5d9ae68461d608fcc7ac30c21fe46daf
yvngjordi <61796788+yvngjordi@users.noreply.github.com>
2024-02-28 16:48:28 -0800

    Update GoogleMap.tsx

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -6,11 +6,10 @@ type GoogleMapProps = {
   height?: string | number;
   width?: string | number;
   frame?: string | number;
-  scrolling?: boolean;
   borderRadius?: number | string;
 };
 
-export const GoogleMap: React.FC<GoogleMapProps> = ({ address, title = "", height, width, frame, scrolling, borderRadius }) => {
+export const GoogleMap: React.FC<GoogleMapProps> = ({ address, title = "", height, width, frame, borderRadius }) => {
   const query = encodeURIComponent(`${address} (${title})`);
 
   const src = `https://maps.google.com/maps?width=100%&height=600&hl=en&q=${query}&t=&z=14&ie=UTF8&iwloc=B&output=embed`;
@@ -21,7 +20,6 @@ export const GoogleMap: React.FC<GoogleMapProps> = ({ address, title = "", heigh
         width={width || '100%'}
         height={height || '600'}
         frameBorder={frame || '0'}
-        scrolling={scrolling}
         marginHeight={0}
         marginWidth={0}
         src={src}
